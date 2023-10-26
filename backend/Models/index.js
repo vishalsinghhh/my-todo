043ad4d308981e39607e260c1a0a03f8195e1555
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-26 22:28:40 +0530

    vercel POSTGRES

--- backend/Models/index.js
+++ backend/Models/index.js
@@ -1,20 +1,35 @@
-const {Sequelize, DataTypes} = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DR_URI, {dialect: "postgres"})
+// const sequelize = new Sequelize(process.env.DR_URI, {dialect: "postgres"})
+
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
+  port: 5432,
+  ssl: true, // Enable SSL
+  dialectOptions: {
+    ssl: {
+      require: true, // Force SSL
+    },
+  },
+});
 
 //checking if connection is done
-sequelize.authenticate().then(() => {
-    console.log(`Database connected to discover`)
-}).catch((err) => {
-    console.log(err)
-})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(`Database connected to discover`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const db = {}
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 //connecting to model
-db.users = require('./userModel') (sequelize, DataTypes)
+db.users = require("./userModel")(sequelize, DataTypes);
 
 //exporting the module
-module.exports = db
+module.exports = db;
