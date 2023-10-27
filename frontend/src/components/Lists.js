69155c2b0b07f6aee505af310896117a50e6d1b7
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-10-27 21:09:06 +0530

    todo list fetch

---
+++ frontend/src/components/Lists.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Lists.css"
+
+const Lists = ({data}) => {
+    console.log(data);
+  return (
+    <div className='lists'>Lists</div>
+  )
+}
+
+export default Lists
--- frontend/src/pages/MyTodo.js
+++ frontend/src/pages/MyTodo.js
@@ -1,25 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
 import { useAppContext } from "../context/appContext";
+import Lists from "../components/Lists";
+import "./MyTodo.css";
 
 const MyTodo = () => {
-  const {getAllLists} = useAppContext()
-  const [lists, setLists] = useState()
+  const { getAllLists } = useAppContext();
+  const [lists, setLists] = useState();
 
-  useEffect(()=>{
-    const fn = async()=>{
-      const res = await getAllLists()
+  useEffect(() => {
+    const fn = async () => {
+      const res = await getAllLists();
       setLists(res);
-    }
-    fn()
-  }, [])
-  
-  console.log(lists);
-  return (
-    <div>
-    <Navbar/>
-    </div>
-  )
-}
+    };
+    fn();
+  }, []);
 
-export default MyTodo
+  console.log(lists?.lists);
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="content">
+        <div className="listMain">
+          {lists?.lists.map((item, i) => {
+            return (
+              <div key={i}>
+                <Lists data={item} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyTodo;
